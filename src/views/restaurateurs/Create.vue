b7250f759ae7e401c3f3092b654d49cb51a98011
Gabriel Amadé <gabrielamade@icloud.com>
2022-07-21 11:54:09 +0200

    nouvelles vues

---
+++ src/views/restaurateurs/Create.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="register">
+    <h2 class="titleRegister">Veuillez-vous inscrire sur cette page</h2>
+
+    <div class="wrapper">
+      <div id="mainContainer" class="mainContainer">
+        <form @submit.prevent="register">
+          <div class="input-container">
+            <label for="lastname"></label>
+            <input
+              type="text"
+              class="lastnameRegister"
+              id="lastname"
+              v-model="lastname"
+              placeholder="Nom"
+              required
+            />
+          </div>
+
+          <div class="input-container">
+            <label for="firstname"></label>
+            <input
+              type="text"
+              class="firstnameRegister"
+              id="firstname"
+              v-model="firstname"
+              placeholder="Prénom"
+              required
+            />
+          </div>
+
+          <div class="input-container">
+            <label for="siret"></label>
+            <input
+              type="number"
+              class="siretRegister"
+              id="siret"
+              v-model="siret"
+              placeholder="Numéro Siret"
+              required
+            />
+          </div>
+
+          <div class="input-container">
+            <label for="email"></label>
+            <input
+              type="email"
+              class="emailRegister"
+              id="email"
+              v-model="email"
+              placeholder="Votre email"
+              required
+            />
+          </div>
+
+          <div class="input-container">
+            <label for="phone"></label>
+            <input
+              type="text"
+              class="phoneRegister"
+              id="phone"
+              v-model="phone"
+              placeholder="Téléphone"
+              required
+            />
+          </div>
+
+          <div class="input-container">
+            <label for="password"></label>
+            <input
+              type="password"
+              class="passwordRegister"
+              id="password"
+              v-model="password"
+              placeholder="Mot de passe"
+              required
+            />
+          </div>
+
+          <div class="boxButton">
+            <input
+              class="register-button2"
+              type="submit"
+              value="Valider Inscription"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Create",
+  components: {},
+
+  data() {
+    return {
+      lastname: "",
+      firstname: "",
+      siret: "",
+      email: "",
+      phone: "",
+      password: "",
+    };
+  },
+
+  methods: {
+    async register() {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          lastname: this.lastname,
+          firstname: this.firstname,
+          siret: this.siret,
+          email: this.email,
+          phone: this.phone,
+          password: this.password,
+        }),
+      };
+
+      const response = await fetch(
+        "http://127.0.0.1:8000/api/restaurateurs/create",
+        options
+      );
+
+      const data = await response.json();
+    },
+  },
+};
+</script>
