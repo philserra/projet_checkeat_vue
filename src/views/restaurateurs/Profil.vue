f6174f049e1cd7bb810f036918e09ce5412707ae
GonzalezC7 <cyril_c7@hotmail.fr>
2022-07-26 11:30:56 +0200

    reactualisation

--- src/views/restaurateurs/Profil.vue
+++ src/views/restaurateurs/Profil.vue
@@ -2,26 +2,24 @@
   <h1>Profil</h1>
 
   <div>
-    <p>{{ id }}</p>
     <p>{{ lastname }}</p>
     <p>{{ firstname }}</p>
     <p>{{ siret }}</p>
     <p>{{ email }}</p>
     <p>{{ phone }}</p>
     <p>{{ password }}</p>
+
     <button @click="deleteProfil">Supprimer</button>
   </div>
   <p>{{ message }}</p>
 
-  <button @click="editProfil()">Modifier</button>
+  <button @click="editProfil">Modifier</button>
 </template>
 
 <script>
-import { getProfil } from "@/lib/profil";
 export default {
   data() {
     return {
-      id: "",
       lastname: "",
       firstname: "",
       siret: "",
@@ -31,8 +29,24 @@ export default {
       message: "",
     };
   },
+
   async mounted() {
-    const profil = await getProfil({ id: 1 });
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    };
+
+    const response = await fetch(
+      "http://127.0.0.1:8000/api/restaurateurs/1",
+      options
+    );
+
+    const data = await response.json();
+
+    const profil = data.restaurateur;
 
     this.lastname = profil.lastname;
     this.firstname = profil.firstname;
@@ -41,30 +55,22 @@ export default {
     this.phone = profil.phone;
     this.password = profil.password;
   },
-  methods: {
-    async editProfil() {
-      await editProfil({
-        id: 1,
-        lastname: this.lastname,
-        firstname: this.firstname,
-        siret: this.siret,
-        email: this.email,
-        phone: this.phone,
-        password: this.password,
-      });
-    },
 
+  methods: {
     async deleteProfil() {
       const options = {
         method: "DELETE",
         headers: {
           "Content-Type": "application/json",
+          Accept: "application/json",
         },
       };
+
       const response = await fetch(
         "http://127.0.0.1:8000/api/restaurateurs/1",
         options
       );
+
       const data = await response.json();
 
       if (data.message == true) {
