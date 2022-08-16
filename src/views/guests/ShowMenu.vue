d37bc2bd55cc15eac6c35b17dd6e5aa4ac9b4817
philserra <psm13@hotmail.fr>
2022-08-16 16:42:04 +0200

    modif menu

--- src/views/guests/ShowMenu.vue
+++ src/views/guests/ShowMenu.vue
@@ -5,76 +5,155 @@
       background-image: url('https://images.unsplash.com/photo-1543007631-283050bb3e8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80');
     "
   >
-    <div class="p-6">
-      <!-- <h1
-      class="text-center text-2xl text-red-600 font-black border-4 border-red-600 shadow-lg rounded p-6"
-    >
-      Liste des restaurants
-    </h1> -->
-      <h1 class="text-red-600 text-3xl p-4 text-center font-black shadow-lg">
-        Choisissez votre menu
-      </h1>
-    </div>
-    <ul>
-      <div
-        class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full p-4"
-      >
-        <li v-for="elem in menu" :key="elem.id">
-          <div class="relative mx-auto w-full">
-            <div
-              class="absolute -inset-1 bg-gradient-to-r from-red-400 to-red-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"
-            ></div>
-
-            <div
-              class="relative inline-block duration-300 ease-in-out transition-transform transform hover:-translate-y-2 w-full"
+    <div class="flex items-center justify-center py-2 px-1">
+      <div class="w-full sm:px-6">
+        <div
+          class="px-1 md:px-10 py-1 md:py-3 bg-black bg-opacity-75 rounded-tl-lg rounded-tr-lg"
+        >
+          <div class="p-2">
+            <h1
+              class="text-red-600 text-3xl p-4 text-center font-black shadow-lg"
             >
-              <div class="shadow-md p-4 rounded-lg bg-black">
-                <div
-                  class="flex justify-center relative rounded-lg overflow-hidden h-52"
-                >
-                  <div
-                    class="transition-transform duration-500 transform ease-in-out hover:scale-110 w-full"
-                  >
-                    <div class="absolute inset-0 bg-gray-100">
-                      <img
-                        class="w-auto rounded-lg"
-                        src="https://images.unsplash.com/photo-1552566626-52f8b828add9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-                        alt=""
-                      />
+              Choisissez votre menu
+            </h1>
+          </div>
+          <ul>
+            <section class="text-gray-600 text-sm">
+              <div class="container px-5 py-24 mx-auto">
+                <div class="flex flex-wrap -m-1">
+                  <div class="p-2 lg:w-1/4">
+                    <div
+                      class="h-full bg-gray-100 bg-opacity-75 px-6 pt-6 pb-24 rounded-lg overflow-hidden text-center relative"
+                    >
+                      <p
+                        class="text-sm font-black md:text-lg text-gray-100 line-clamp-1"
+                      >
+                        Entrées
+                      </p>
+                      <li v-for="elem in menu" :key="elem.id">
+                        <div v-if="elem.category === 'entree'">
+                          <button
+                            @click="
+                              addEntree(elem.name, elem.priceTtc, elem.id)
+                            "
+                            class="inline-block p-1 rounded border-2 border-red-600 text-sm font-black md:text-lg text-gray-100 line-clamp-1"
+                          >
+                            {{ elem.name }}
+                          </button>
+                          <span
+                            class="inline-block text-lg font-black md:text-lg text-gray-100 line-clamp-1"
+                          >
+                            {{ elem.priceTtc }}€
+                          </span>
+                        </div>
+                      </li>
                     </div>
                   </div>
 
-                  <span
-                    class="absolute top-0 left-0 inline-flex mt-3 ml-3 px-3 py-2 rounded-lg z-10 bg-green-300 text-sm font-medium text-white select-none"
-                  >
-                    Recommandé
-                  </span>
-                </div>
-                <div class="mt-4">
-                  <p
-                    class="text-lg font-black md:text-lg text-gray-100 line-clamp-1"
-                  >
-                    <span class="text-red-600 font-bold">Restaurant:</span>
-                    {{ elem.name }}
-                  </p>
-                  <p
-                    class="text-lg font-black md:text-lg text-gray-100 line-clamp-1"
-                  >
-                    {{ elem.category }}
-                  </p>
-                  <p
-                    class="text-lg font-black md:text-lg text-gray-100 line-clamp-1"
-                  >
-                    {{ elem.priceTtc }}
-                  </p>
-                  <p></p>
+                  <div class="p-2 lg:w-1/4">
+                    <div
+                      class="h-full text-sm bg-gray-100 bg-opacity-75 px-16 pt-16 pb-24 rounded-lg overflow-hidden text-center relative"
+                    >
+                      <p
+                        class="text-sm font-black md:text-lg text-gray-100 line-clamp-1"
+                      >
+                        Plats
+                      </p>
+                      <li v-for="elem in menu" :key="elem.id">
+                        <div v-if="elem.category === 'plat'">
+                          <button
+                            class="inline-block p-1 rounded border-2 border-red-600 text-sm font-black md:text-lg text-gray-100 line-clamp-1"
+                            @click="addPlat(elem.name, elem.priceTtc, elem.id)"
+                          >
+                            <span class="inline-block text-red-600 font-bold"
+                              >{{ elem.category }}:
+                            </span>
+                            {{ elem.name }} -
+                            <span
+                              class="inline-block text-sm font-black md:text-lg text-gray-100 line-clamp-1"
+                              >Prix:
+                            </span>
+                            {{ elem.priceTtc }} €
+                          </button>
+                        </div>
+                      </li>
+                    </div>
+                  </div>
+
+                  <div class="p-2 lg:w-1/4">
+                    <div
+                      class="h-full bg-gray-100 bg-opacity-75 px-16 pt-16 pb-24 rounded-lg overflow-hidden text-center relative"
+                    >
+                      <p
+                        class="text-sm font-black md:text-lg text-gray-100 line-clamp-1"
+                      >
+                        Desserts
+                      </p>
+                      <li v-for="elem in menu" :key="elem.id">
+                        <p
+                          class="inline-block text-lg font-black md:text-lg text-gray-100 line-clamp-1"
+                        >
+                          <span class="inline-block text-red-600 font-bold"
+                            >{{ elem.category }}:
+                          </span>
+                          {{ elem.name }} -
+                          <span
+                            class="inline-block text-lg font-black md:text-lg text-gray-100 line-clamp-1"
+                            >Prix:
+                          </span>
+                          {{ elem.priceTtc }} €
+                        </p>
+                      </li>
+                    </div>
+                  </div>
+
+                  <div class="p-2 lg:w-1/4">
+                    <div
+                      class="h-full bg-gray-100 bg-opacity-75 px-16 pt-16 pb-24 rounded-lg overflow-hidden text-center relative"
+                    >
+                      <p
+                        class="text-sm font-black md:text-lg text-gray-100 line-clamp-1"
+                      >
+                        Boissons
+                      </p>
+                      <li v-for="elem in menu" :key="elem.id">
+                        <p
+                          class="inline-block text-lg font-black md:text-lg text-gray-100 line-clamp-1"
+                        >
+                          <span class="inline-block text-red-600 font-bold"
+                            >{{ elem.category }}:
+                          </span>
+                          {{ elem.name }} -
+                          <span
+                            class="inline-block text-lg font-black md:text-lg text-gray-100 line-clamp-1"
+                            >Prix:
+                          </span>
+                          {{ elem.priceTtc }} €
+                        </p>
+                      </li>
+                    </div>
+                  </div>
                 </div>
               </div>
+            </section>
+          </ul>
+          <div class="command border-2 border-red-600 text-gray-100">
+            <p class="">Votre commande</p>
+            <div v-for="elem in command" :key="elem.id">
+              <p class="inline-block">{{ elem }}</p>
+              <button
+                @click="deleteAddition(elem.starter)"
+                class="inline-block text-red-600 border border-red-600"
+              >
+                -
+              </button>
             </div>
+
+            <p v-bind="addition">total: {{ total }}</p>
           </div>
-        </li>
+        </div>
       </div>
-    </ul>
+    </div>
   </div>
 </template>
 
@@ -86,6 +165,9 @@ export default {
     return {
       menu: [],
       id_restaurant: idRestaurantMenu,
+      command: [],
+      price: [0],
+      total: "",
     };
   },
 
@@ -106,6 +188,43 @@ export default {
     const data = await response.json();
 
     this.menu = data.menu;
+    // console.log(this.menu);
+  },
+  computed: {
+    addition() {
+      const sum = this.price.reduce((a, b) => a + b);
+
+      this.total = sum;
+    },
+  },
+  methods: {
+    addEntree(entree, value, id) {
+      const starter = { category: entree, price: value, id: id };
+      // console.log(starter);
+      this.command.push(starter);
+      this.price.push(value);
+
+      // console.log(this.price);
+
+      console.log(this.command);
+    },
+
+    addPlat(plat, value) {
+      const course = plat + " " + value;
+      this.command.push(course);
+      this.price.push(value);
+      // console.log(this.command);
+    },
+
+    deleteAddition(id) {
+      // trouver lors du clique delete
+      // récupérer id du plat
+      // find index
+      // si trouvé alors supprimé
+      if (id === this.command.starter.id) {
+        this.command.starter.id.splice(0, id);
+      }
+    },
   },
 };
 </script>
